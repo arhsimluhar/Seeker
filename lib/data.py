9822736fd9e3ddc655f1291757ad722a236e65d9
rahul1809 <rahul.mishra2003@gmail.com>
2017-02-12 11:09:31 +0530

    revert changes and added one validation function for random ints

--- lib/data.py
+++ lib/data.py
@@ -15,15 +15,19 @@ def randomInt(start = 0 ,end = 100):
 	return random.randint(start,end)
 
 
-def randomAlpha(case = "lower"):
-	'''
+def randomAlpha(start = 97,end = 122, case = "lower"):
+	''' 	
 	returns any random Alphabet between A-Z or a-z (inclusive)
 	if case not specified give lowercase letter
 	'''
 	if case == "lower":
-		return chr(randomInt(97,122))
+		return chr(randomInt(start,end))
+
 	else:
-		return chr(randomInt(65,90))
+		start = 65
+		end = 90
+		return chr(randomInt(start,end))
+
 
 
 def randomAscii(start = 0,end = 255,range = True):
@@ -33,23 +37,23 @@ def randomAscii(start = 0,end = 255,range = True):
 	if not range:
 		return chr(randomInt(start,end))
 	else:
-		return map(chr,randomInts(start,end))  # what is the use of map() if randomInts() will generate only one output ?
+		return map(chr,randomInts(start,end))  # randInts will generate random numbers in ASCII range and map will apply chr function to convert them to characters,remove this comment after reading
 
 
 
 def randomInts(start = 0, end = 100,count = 100, unique = True):
-    '''
-    returns list of integers and uniqueness as parameter to
-    allow duplicate entries in the list or not.
-    '''
-    #if unique numbers are required
-    if unique:
-        try:
-           return random.sample(range(start,end),count)
-        except ValueError:
-           print('Sample size exceeded population size.')
-    else:
-        ans = []
-        for i in xrange(count):
-            ans.append(randomInt(start,end))
-        return ans
+	'''
+	returns list of integers and uniqueness as parameter to
+	allow duplicate entries in the list or not.
+	'''
+	#if unique numbers are required
+	if unique:
+		try:
+		   return random.sample(range(start,end),count)
+		except ValueError:
+		   print('Sample size exceeded population size.')
+	else:
+		ans = []
+		for i in xrange(count):
+			ans.append(randomInt(start,end))
+		return ans
